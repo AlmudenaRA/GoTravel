327e44079be9a71f79cfe1b3e445ebbaa395fdfb
Almudena Ruedas <almudena.ruedas@gmail.com>
2022-04-12 17:58:06 +0200

    Modificación splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gotravel/page/splash_screen.dart';
+import 'package:gotravel/theme/my_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,33 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GoTravel',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        backgroundColor: MyColors.background,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/theme/my_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color background = const Color(0xFF424242);
+  static const Color secundary = const Color(0xFF790e8b);
+}

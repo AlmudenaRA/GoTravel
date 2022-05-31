64e4fded8e7787177d17f64efb42232fca013785
Almudena Ruedas <almudena.ruedas@gmail.com>
2022-05-31 18:29:38 +0200

    creada la barra de navegación

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gotravel/src/core/constants.dart';
+import 'package:gotravel/src/pages/favorite_page.dart';
+import 'package:gotravel/src/pages/profile_page.dart';
+import 'package:gotravel/src/theme/my_colors.dart';
+import 'package:gotravel/src/utils/utils.dart' as utils;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -8,13 +14,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _auth = FirebaseAuth.instance;
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    MyHomePage(),
+    FavoritePage(),
+    ProfilePage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text("Prueba"),
+      body: _widgetOptions[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: MyColors.pLight,
+        selectedItemColor: MyColors.secundary,
+        unselectedItemColor: MyColors.textWhite,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: Constants.bAppBarHome,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: Constants.bAppBarFav,
+          ),
+          BottomNavigationBarItem(
+              icon: CircleAvatar(
+                backgroundImage: utils.pickImage(_auth),
+                radius: 13,
+              ),
+              label: '${_auth.currentUser!.displayName}'),
+        ],
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
